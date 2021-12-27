38d1ceca49c3b1385611c9d27e523066dbe36c2f
Ian Lovice <ianlovice@Ians-MacBook-Air.local>
2021-12-27 18:23:27 -0500

    genius connor

--- client/src/assets/data/experienceData.js
+++ client/src/assets/data/experienceData.js
@@ -5,9 +5,9 @@ const experienceData = [
     description: "During my time at General Assembly, I was immersed in a variety of software development technologies and languages, including HTML, CSS, Javascript, API's, SQL, Ruby, Ruby on Rails, React, Heroku, Github, Github Enterprise, Markdown, NPM, Express, and Mongo. Over 400 hours of professional training in 12 weeks, I used a hands-on approach to designing and building web applications with full CRUD functionality."
   },
   {
-    company: "St Johns Fire District",
+    company: "St. John's Fire District",
     title: "Firefighter",
-    description: "Served the Johns Island community by protecting lives and property during emergent situations. Worked within a team to gain control over emergency scenes. Participated in fire-prevention programs with elementary school students."
+    description: "Served the Johns Island community by protecting lives and property during emergency situations. Worked within a team to gain control over emergency scenes. Participated in fire-prevention programs with elementary school students."
   },
   {
     company: "Seabrook Island Property Owner's Association",
--- client/src/assets/data/projectData.js
+++ client/src/assets/data/projectData.js
@@ -20,7 +20,7 @@ const projectData = [
     image: "fransklist",
     github_url: "https://github.com/ianlov/frankslist",
     url: "https://franks-list.netlify.app/",
-    description: "Frankslist is the one stop web application for finding your dream hobby. Create an account, search for hobbies, and save your favorites.",
+    description: "Frankslist is the one-stop web application for finding your dream hobby. Create an account, search for hobbies, and save your favorites.",
     tech: <div className="tech" ><i class="devicon-react-original"></i><i class="devicon-nodejs-plain"></i><i class="devicon-express-original"></i><i class="devicon-mongodb-plain"></i></div>
   },
   {
