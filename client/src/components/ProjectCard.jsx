2944e783c455eb4473ebdf57e0da436eedc2a840
Ian Lovice <ianlovice@Ians-MacBook-Air.local>
2021-12-27 13:50:27 -0500

    almost there

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -3,12 +3,11 @@ import "../assets/css/projectcard.css";
 const ProjectCard = (props) => {
   return (
     <>
-      <div className="project-card" >
-        <img src={props.src} alt={props.project.image} />
-        <a href={`${props.project.github_url}`} ><h2>{props.project.title} <i class="devicon-github-original"></i></h2></a>
+      <a href={`${props.project.url}`} className="project-card" >
+        <h2>{props.project.title}</h2>
         <p>{props.project.description}</p>
         {props.project.tech}
-      </div>
+      </a>
     </>
   )
 }
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,19 +4,15 @@ import SectionHeader from "./SectionHeader.jsx";
 import ProjectCard from "./ProjectCard.jsx";
 import projectData from "../assets/data/projectData.js";
 
-import livemusicunderground from "../assets/images/livemusicunderground.png";
-import frankslist from "../assets/images/frankslist.png";
-import wikiroll from "../assets/images/wikiroll.png";
-
 const Projects = () => {
   return (
     <>
       <div className="projects" id="projects" >
         <SectionHeader section={"Projects"} />
         <div className="projects-container" >
-          <ProjectCard project={projectData[0]} src={livemusicunderground} />
-          <ProjectCard project={projectData[1]} src={frankslist} />
-          <ProjectCard project={projectData[2]} src={wikiroll} />
+          <ProjectCard project={projectData[0]} />
+          <ProjectCard project={projectData[1]} />
+          <ProjectCard project={projectData[2]} />
         </div>
       </div>
     </>
